1802a37426c6b3a6bbb69bcc696cef25a2860f29
Nicestman <ouattaraali941@gmail.com>
2026-05-11 23:55:43 +0000

    creation du chemin vers le fichier a lire

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Chemin vers le fichier à lire
+        File fichier = new File("test.txt");
+        int totalMots = 0;
+
+
+    }
+}
